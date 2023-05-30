f0f11913c3203f36d7e1a7db1b350e2bbcebd025
Stanislav Cernioglo <stanislav.cernioglo@amdaris.com>
2023-05-30 21:09:04 +0300

    Add URL

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const config = {
+    apiUrl: process.env.REACT_APP_API_URL || 'https://localhost:7091',
+  };
+  
+  export default config;
--- src/shared/api/organization/organizationService.ts
+++ src/shared/api/organization/organizationService.ts
@@ -2,29 +2,32 @@ import axios from "axios";
 import { OrganizationListDto } from "shared/models/organizationModel";
 import { PagedRequest, PagedResult } from "shared/models/pagedRequestModel";
 import { MyProjectDto } from "shared/models/projectModel";
+import config from "config";
+
+const apiUrl = config.apiUrl;
 
 export async function getPagedOrganizations(pagedRequest: PagedRequest) {
-    let response = await axios.post<PagedResult<OrganizationListDto>>("https://localhost:7091/api/organizations/paginated-search", pagedRequest );
+    let response = await axios.post<PagedResult<OrganizationListDto>>(`${apiUrl}/api/organizations/paginated-search`, pagedRequest );
     return response.data;
 }
 
 export async function getMyProjects() {
-    let response = await axios.get<MyProjectDto[]>("https://localhost:7091/api/projects/mine");
+    let response = await axios.get<MyProjectDto[]>(`${apiUrl}/api/projects/mine`);
     return response.data;
 }
 
 export async function removeParticipant(id?: number) {
-    let response = await axios.delete(`https://localhost:7091/api/projectvolunteers/remove/${id}`);
+    let response = await axios.delete(`${apiUrl}/api/projectvolunteers/remove/${id}`);
     return response.data;
 }
 
 export async function confirmParticipation(id?: number) {
-    let response = await axios.post(`https://localhost:7091/api/projectvolunteers/confirm/${id}`, id );
+    let response = await axios.post(`${apiUrl}/api/projectvolunteers/confirm/${id}`, id );
     return response.data;
 }
 
 export async function cancelConfirmation(id?: number) {
-    let response = await axios.post(`https://localhost:7091/api/projectvolunteers/cancel/${id}`, id );
+    let response = await axios.post(`${apiUrl}/api/projectvolunteers/cancel/${id}`, id );
     return response.data;
 }
 
